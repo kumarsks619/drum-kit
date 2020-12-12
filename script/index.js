2a0ca6e34bbe8b7f7e94435045bb18671d66fb6d
VeNoM <kumarsks619@gmail.com>
2020-12-12 11:12:08 +0530

    Goin to deploy

---
+++ script/index.js
@@ -0,0 +1,59 @@
+// Detecting Button clicks
+for (let i = 0; i < document.querySelectorAll('.drum').length; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', (e) => {
+        let btnText = e.target.innerHTML.toLowerCase()
+        playSound(btnText)
+        animateBtn(btnText)
+    })
+}
+
+// Detecting Keyboard press
+document.addEventListener('keydown', (e) => {
+    playSound(e.key)
+    animateBtn(e.key)
+})
+
+// Function to play corresponding sound
+const playSound = (key) => {
+    switch (key) {
+        case "w":
+            let tom1 = new Audio('./sounds/tom-1.mp3')
+            tom1.play()
+            break
+        case "a":
+            let tom2 = new Audio('./sounds/tom-2.mp3')
+            tom2.play()
+            break
+        case "s":
+            let tom3 = new Audio('./sounds/tom-3.mp3')
+            tom3.play()
+            break
+        case "d":
+            let tom4 = new Audio('./sounds/tom-4.mp3')
+            tom4.play()
+            break
+        case "j":
+            let snare = new Audio('./sounds/snare.mp3')
+            snare.play()
+            break
+        case "k":
+            let crash = new Audio('./sounds/crash.mp3')
+            crash.play()
+            break
+        case "l":
+            let kick = new Audio('./sounds/kick-bass.mp3')
+            kick.play()
+            break
+        default:
+            console.log(key)
+    }
+}
+
+// Function to change button css that is being clicked
+const animateBtn = (key) => {
+    let activeBtn = document.querySelector(`.${key}`)
+    activeBtn.classList.add('pressed')
+    setTimeout(() => {
+        activeBtn.classList.remove('pressed')
+    }, 100)
+}
